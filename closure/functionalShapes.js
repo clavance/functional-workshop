293fdbe4adede471d72b9c93765388564ea592bb
Clavance Lim <clavance@outlook.com>
2020-08-09 18:19:29 +0100

    add tests for pure functions, add working functions

--- closure/functionalShapes.js
+++ closure/functionalShapes.js
@@ -1,26 +1,37 @@
-'use strict';
+"use strict";
 
 const rectangle = (height, width) => {
   return (method) => {
     switch (method) {
-      case 'getWidth': return () => width;
-      case 'getHeight': return  //TODO
-      case 'getArea': return //TODO
-      case 'setWidth': return (newWidth) => rectangle(height, newWidth);
-      case 'setHeight': return  //TODO
-      case 'toString': return () => new Array(height+1).join(new Array(width+1).join('X ') + '\n');
-      default: return (...args) => `unknown method "${method}"`;
+      case "getWidth":
+        return () => width;
+      case "getHeight":
+        return; //TODO
+      case "getArea":
+        return; //TODO
+      case "setWidth":
+        return (newWidth) => rectangle(height, newWidth);
+      case "setHeight":
+        return; //TODO
+      case "toString":
+        return () =>
+          new Array(height + 1).join(new Array(width + 1).join("X ") + "\n");
+      default:
+        return (...args) => `unknown method "${method}"`;
     }
-  }
+  };
 };
 
 const square = (side) => {
   return (method) => {
-    if (method === 'setSize' || method === 'setWidth' || method === 'setHeight') {
-      return (size) => // TODO
-    }
-    else return //TODO
-  }
+    if (
+      method === "setSize" ||
+      method === "setWidth" ||
+      method === "setHeight"
+    ) {
+      return (size) => {}; // TODO
+    } else return; //TODO
+  };
 };
 
 module.exports = { rectangle, square };
--- pure-functions/impureToPure.js
+++ pure-functions/impureToPure.js
@@ -1,19 +1,64 @@
-'use strict';
+"use strict";
+
+var test = require("tape");
+
+// The refactored pure function should pass these tests
+test("setPrice()", (assert) => {
+  const msg = "setPrice() should set the price in the given item object.";
+  const immutable = "original should not be mutated";
+
+  const item = {
+    name: "test",
+    price: 30,
+  };
+  const copy = Object.assign({}, item);
+
+  const actual = setPrice(item, 50);
+  const expected = {
+    name: "test",
+    price: 50,
+  };
+
+  assert.deepEqual(actual, expected, msg);
+  assert.deepEqual(item, copy, immutable);
+  assert.end();
+});
+
+test("addToCart()", (assert) => {
+  const msg = "should add item to cart";
+  const immutable = "original should not be mutated";
+
+  const originalCart = [];
+  const item = {
+    name: "Violator",
+    price: 30,
+  };
+  const copy = originalCart.slice();
+
+  const actual = addToCart(originalCart, item);
+  const expected = [item];
+
+  assert.deepEqual(actual, expected, msg);
+  assert.deepEqual(originalCart, copy, immutable);
+  assert.end();
+});
 
 // TODO Copy the impure version of your function here:
 
+// setPrice(item: Object, price: Number) => item: Object
+// const setPrice = (item, price) => Object.assign(item, { price });
 
-
-
-
-
-
-
-
-
-
-
-
-
+// addToCart(cart: Array, item: Object) => cart: Array
+// const addToCart = (cart, item) => {
+//   cart.push(item);
+//   return cart;
+// };
 
 // TODO Now write a refactored, pure version of the function:
+const setPrice = (item, price) => {
+  return { ...item, price };
+};
+
+const addToCart = (cart, item) => {
+  return [...cart, item];
+};
